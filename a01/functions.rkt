a5c755ea7b18ed790582ccbdbf9e8382319afd78
San1tater <98987733+San1tater1122@users.noreply.github.com>
2026-09-18 17:47:12 -0400

    Add files via upload

--- a01/functions.rkt
+++ a01/functions.rkt
@@ -1,7 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname a01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; a01 assignment part B
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname functions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; *************************
+;; Jinghao Zhuang (21238127)
+;; CS 135 Fall 2026
+;; Assignt a01, Problem B
+;; *************************
+
 ;; (a)
 (define (doughnut-surface-area r z) (* 4 pi pi r z))
 
